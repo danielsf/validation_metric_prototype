118dc39b1fff0062e665685e4149137f219780cb
danielsf <scott.f.daniel@gmail.com>
2018-06-21 21:19:01 +0000

    example.py actually adds data request

--- example.py
+++ example.py
@@ -9,3 +9,5 @@ if __name__ == "__main__":
     ct_metric = CtMetricContainer()
     ct_metric.metric_yaml = "metrics/ct_metric.yaml"
     ct_metric.specs_dir = "metrics/ct_metric"
+    data_request = ('src', {'filter':'HSC_Y', 'ccd':28, 'visit':374})
+    ct_metric.add_data_request(data_request)
