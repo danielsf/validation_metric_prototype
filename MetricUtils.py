0f8f7e4c11745cbe45d6b4a8fdfa0176c0296e53
danielsf <scott.f.daniel@gmail.com>
2018-06-21 20:24:29 +0000

    use json to compare dicts

--- MetricUtils.py
+++ MetricUtils.py
@@ -1,18 +1,15 @@
+import json
+
 __all__ = ["are_data_requests_identical"]
 
 def are_data_requests_identical(t1, t2):
     if t1[0] != t2[0]:
         return False
 
-    k1 = list(t1[1].keys()).sort()
-    k2 = list(t2[1].keys()).sort()
-
-    if k1 != k2:
+    d1 = json.dumps(t1[1], sort_keys=True)
+    d2 = json.dumps(t2[1], sort_keys=True)
+    if d1 != d2:
         return False
-
-    for k in k1.keys():
-        if k1[k] != k2[k]:
-            return False
 
     return True
 
