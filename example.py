3a88eaf2b9c009a1f1380eea66aa8eb9c57a321c
danielsf <scott.f.daniel@gmail.com>
2018-06-21 22:23:37 +0000

    add a full focal plane source count metric as well

--- example.py
+++ example.py
@@ -1,22 +1,44 @@
+import json
 import astropy.units as astropy_units
 import lsst.verify as lsst_verify
 import lsst.daf.persistence as daf_persistence
 from MetricContainerModule import MetricContainer
 from JobDriverModule import JobDriver
 
-class CtMetricContainer(MetricContainer):
+class CtMetricCCD(MetricContainer):
 
     def do_measurement(self, data_dict):
         data = data_dict[self.data_request[0]]
         src_ct = len(data['coord_ra'])
         ct_meas = lsst_verify.Measurement('ct_metric.SrcCts',
                                           src_ct*astropy_units.dimensionless_unscaled)
-        print('measured source count %d' % src_ct)
+
+        data_dict = json.loads(self.data_request[0][1])
+        i_ccd = data_dict['ccd']
+        print('measured %d source count %d' % (i_ccd, src_ct))
         job = lsst_verify.Job.load_metrics_package()
         job.metrics.update(self._metric_set)
         job.specs.update(self._specs_set)
         job.measurements.insert(ct_meas)
         self._measurement = job
+
+class CtMetricFP(MetricContainer):
+
+    def do_measurement(self, data_dict):
+        src_ct = 0
+        for data_id in self.data_request:
+            data = data_dict[data_id]
+            src_ct += len(data['coord_ra'])
+
+        ct_meas = lsst_verify.Measurement('ct_metric.SrcCts',
+                                          src_ct*astropy_units.dimensionless_unscaled)
+        print('measured fp source count %d' % src_ct)
+        job = lsst_verify.Job.load_metrics_package()
+        job.metrics.update(self._metric_set)
+        job.specs.update(self._specs_set)
+        job.measurements.insert(ct_meas)
+        self._measurement = job
+
 
 if __name__ == "__main__":
 
@@ -25,14 +47,20 @@ if __name__ == "__main__":
     job_driver = JobDriver()
     job_driver.butler =butler
 
+    fp_metric = CtMetricFP()
+    fp_metric.metric_yaml = "metrics/ct_metric.yaml"
+    fp_metric.specs_dir = "metrics/ct_metric"
+
     for i_ccd in range(29):
         data_id = {'filter':'HSC-Y', 'ccd':i_ccd, 'visit':374}
         if butler.datasetExists('src', dataId=data_id):
-            ct_metric = CtMetricContainer()
-            ct_metric.metric_yaml = "metrics/ct_metric.yaml"
-            ct_metric.specs_dir = "metrics/ct_metric"
+            ccd_metric = CtMetricCCD()
+            ccd_metric.metric_yaml = "metrics/ct_metric.yaml"
+            ccd_metric.specs_dir = "metrics/ct_metric"
             data_request = ('src', data_id)
-            ct_metric.add_data_request(data_request)
-            job_driver.add_metric(ct_metric)
+            ccd_metric.add_data_request(data_request)
+            fp_metric.add_data_request(data_request)
+            job_driver.add_metric(ccd_metric)
 
+    job_driver.add_metric(fp_metric)
     job_driver.run()
