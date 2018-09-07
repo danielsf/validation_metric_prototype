4422578788924899d1a106e561dd33d34ce574fe
danielsf <scott.f.daniel@gmail.com>
2018-09-07 14:41:03 +0000

    add print statement

--- get_valid_data_id.py
+++ get_valid_data_id.py
@@ -8,10 +8,13 @@ possible_id = butler.queryMetadata('src', ['visit', 'ccd', 'filter', 'field'])
 
 ct = 0
 with open('valid_hsc_id.txt', 'w') as out_file:
-    for pid in possible_id:
+    for i_id, pid in enumerate(possible_id):
         data_id = {'visit':pid[0], 'ccd':pid[1], 'filter':pid[2], 'field':pid[3]}
         if butler.datasetExists('src', dataId=data_id):
             out_file.write('%s\n' % json.dumps(data_id))
             ct+=1
+
+        if i_id%1000==0:
+            print('did %d of %d' % (i_id, len(possible_id)))
 
 print('all done')
